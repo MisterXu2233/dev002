3d19c8acafd95ec6b683059fe63ba7185871f154
xwh <1440325490@qq.com>
2022-03-10 21:17:14 +0800

    第一次提交

--- 02-MyBatis-13-Dynamic-SQL/src/main/java/com/fc/dao/StudentDao.java
+++ 02-MyBatis-13-Dynamic-SQL/src/main/java/com/fc/dao/StudentDao.java
@@ -5,11 +5,13 @@ import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
-public abstract class StudentDao {
-    public abstract List<Student> findAll();
+public interface StudentDao {
+    List<Student> findAll();
 
-    //模糊查询
-    public abstract List<Student> findByKeyword(@Param("name") String name, @Param("age") Integer age);
-    public abstract List<Student> findByStudent(Student student);
-    public abstract int update(Student student);
+    // 模糊查询
+    List<Student> findByKeyword(@Param("name") String name, @Param("age") Integer age);
+
+    List<Student> findByStudent(Student student);
+
+    int update(Student student);
 }
